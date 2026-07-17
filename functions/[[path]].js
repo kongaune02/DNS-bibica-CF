b390623a9e2f2c08d57fe637755030413b442d92
kongaune02 <43430764+kongaune02@users.noreply.github.com>
2026-07-18 00:07:12 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,6 +1,6 @@
 // ==================== CONFIG ====================
-const UPSTREAM_PRIMARY = 'https://bu0eg1tdzu.cloudflare-gateway.com/dns-query';
-const UPSTREAM_FALLBACK = 'https://rhpcv957tj.cloudflare-gateway.com/dns-query';
+const UPSTREAM_PRIMARY = 'https://pbrhb41xnj.cloudflare-gateway.com/dns-query';
+const UPSTREAM_FALLBACK = 'https://doh.bibica.net/dns-query';
 const UPSTREAM_GEO_BYPASS = 'https://dns.mullvad.net/dns-query'; // Re-resolve without ECS when geo-block returns loopback
 const UPSTREAM_TIMEOUT = 5000;
 
